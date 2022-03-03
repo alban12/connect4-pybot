4c89c7321932a7b4be32199076723b2c46831a15
alban12 <48073591+alban12@users.noreply.github.com>
2022-03-03 23:49:41 +0100

    Update make_sub.py

--- make_sub.py
+++ make_sub.py
@@ -4,6 +4,6 @@ import click
 @click.option('--weights-path', help='The path to the weights of the network.')
 @click.option('--model-path', prompt='the path to the model')
 def make_sub(weights_path, model_path):
-  ...
+  
   
 make_sub()
