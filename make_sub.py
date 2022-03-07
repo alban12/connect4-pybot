d9a0c051e8ed7ca3e45599ca4ec504c0bfac5827
alban12 <48073591+alban12@users.noreply.github.com>
2022-03-07 23:15:20 +0100

    Update make_sub.py

--- make_sub.py
+++ make_sub.py
@@ -1,9 +1,13 @@
 import click
+from zipfile import ZipFile
 
 @click.command()
 @click.option('--weights-path', help='The path to the weights of the network.')
 @click.option('--model-path', prompt='the path to the model')
 def make_sub(weights_path, model_path):
+  zipObj = ZipFile('sub.zip', 'w')
+  zipObj.write(f'{weights_path}')
+  zipObj.write(f'{model_path}')
   
   
 make_sub()
